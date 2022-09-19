f8e189d2d4735fb2574f04258f6ea0bdec378d90
Łukasz Stępień <lukaszatepien@gmail.com>
2022-09-19 12:04:43 +0200

    Working ex.1

---
+++ zadanie_1/app.js
@@ -0,0 +1,9 @@
+const a = document.getElementById("a")
+const b = document.getElementById("b")
+const button = document.getElementsByTagName("a")[0]
+
+
+button.addEventListener("click", (e) => {
+    document.getElementById("result").value = a.value * b.value
+    e.preventDefault()
+}, false)
