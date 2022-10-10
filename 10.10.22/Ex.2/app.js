f06399dab743624009196bc70faef1693a246469
Łukasz Stępień <lukaszatepien@gmail.com>
2022-10-10 14:03:23 +0200

    Less code

--- 10.10.22/Ex.2/app.js
+++ 10.10.22/Ex.2/app.js
@@ -1,15 +1,9 @@
 let randomNumber = [];
 let temp = 0;
-let repeat = false
+let repeat = true
 
 for (let i = 0; i < 6; i++) {
-    temp = Math.floor(Math.random() * 7 + 1)
-    for (let a = 0; a < i; a++) {
-        if (randomNumber[a] == temp) {
-            repeat = true
-        }
-    }
-    while (repeat == true) {
+    do {
         repeat = false;
         temp = Math.floor(Math.random() * 7 + 1)
         for (let a = 0; a < i; a++) {
@@ -18,7 +12,7 @@ for (let i = 0; i < 6; i++) {
             }
             
         }
-    }
+    } while (repeat == true);
     randomNumber[i] = temp;
     document.write(`${randomNumber[i]}, `)
 }
