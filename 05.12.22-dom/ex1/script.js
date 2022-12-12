d2625b5c5db0219ae0e465e7fd561b493594a1d5
Łukasz Stępień <lukaszatepien@gmail.com>
2022-12-12 12:31:12 +0100

    Done ex1

--- 05.12.22-dom/ex1/script.js
+++ 05.12.22-dom/ex1/script.js
@@ -1,4 +1,16 @@
-const  li = document.querySelectorAll('li');
-document.querySelector('#search').addEventListener('input' (event) => {
+async function name(params) {
+    
+}
 
-});
+document.querySelector('#search').addEventListener('keyup', (event) => {
+    const text = event.target.value.toLowerCase();
+    const lis = document.querySelectorAll('li');
+
+    for (let i = 0; i < lis.length; i++) {
+        if (lis[i].textContent.toLowerCase().indexOf(text) !== -1) {
+            lis[i].style.display = 'block';
+        } else {
+            lis[i].style.display = 'none';
+        }
+    }
+})
