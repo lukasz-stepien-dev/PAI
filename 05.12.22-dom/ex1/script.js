c0de53432530237c6cd4bc2ad541fa7e48e1f8ac
Łukasz Stępień <lukaszatepien@gmail.com>
2022-12-12 12:53:24 +0100

    Added forEach

--- 05.12.22-dom/ex1/script.js
+++ 05.12.22-dom/ex1/script.js
@@ -1,16 +1,24 @@
-async function name(params) {
-    
-}
-
 document.querySelector('#search').addEventListener('keyup', (event) => {
     const text = event.target.value.toLowerCase();
     const lis = document.querySelectorAll('li');
+    let everythingNone = true
 
-    for (let i = 0; i < lis.length; i++) {
-        if (lis[i].textContent.toLowerCase().indexOf(text) !== -1) {
-            lis[i].style.display = 'block';
+    //for (let i = 0; i < lis.length; i++) {
+    //    if (lis[i].textContent.toLowerCase().indexOf(text) !== -1) {
+    //        setTimeout(() => {lis[i].style.display = 'block';}, 1000);
+    //    } else {
+    //        setTimeout(() => {lis[i].style.display = 'none';}, 1000);
+    //        
+    //    }
+    //}
+
+    lis.forEach(el => {
+        if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+            el.style.display = 'block';
         } else {
-            lis[i].style.display = 'none';
+            el.style.display = 'none';
         }
-    }
+    })
+
+    
 })
