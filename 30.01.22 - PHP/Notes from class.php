69d0a6f304f4f0ab3bd2df717a0b934a312ff766
Łukasz Stępień <lukaszatepien@gmail.com>
2023-01-30 14:25:25 +0100

    Add code

---
+++ 30.01.22 - PHP/Notes from class.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    //Zmienne i stałe
+    define("imie", "nazwisko");
+    const rok = 1991;
+    $zawod = "informatyk";
+    $wiek  = 31;
+    print(imie."<br>");
+    print($zawod."<br>\n");
+    print("rocznik - ".rok."<br>");
+    print($wiek."lat <br>");
+
+    echo "<hr>";
+
+    // Operatory
+    $a = 4;
+    $b = 2;
+    $suma = $a + $b;
+    $wyr = (($a+$b) * $b) / (($a+$b) / ($a-$b));
+    $pierwiastek = sqrt($a);
+    $potega = $a**$b;
+    echo "Suma liczb wynosi $suma<br>";
+    echo "Wartość wyrażenia wynosi $wyr<br>";
+    echo "Pierwiastek wynosi $pierwiastek<br>";
+    echo "Potega wynosi $potega<br>";
+    ?>
+</body>
+</html>
